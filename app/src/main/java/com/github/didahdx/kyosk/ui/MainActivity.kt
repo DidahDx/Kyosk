ce32306fa55cd7ba419136ec4ee3b5d68a1590f1
DanielDidah <didahdaniel@gmail.com>
2021-10-20 15:37:55 +0300

    initial project setup

---
+++ app/src/main/java/com/github/didahdx/kyosk/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.didahdx.kyosk.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.didahdx.kyosk.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
