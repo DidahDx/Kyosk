f781ea725a99f5abe817fdeae9bba437f57fafd7
DanielDidah <didahdaniel@gmail.com>
2021-10-20 23:18:52 +0300

    added initial layout

---
+++ app/src/main/java/com/github/didahdx/kyosk/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.didahdx.kyosk.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/github/didahdx/kyosk/ui/payment/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.didahdx.kyosk.ui.payment
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
