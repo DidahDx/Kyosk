38e7f5e951c883399b562e877f8d226fef187ad3
DanielDidah <didahdaniel@gmail.com>
2021-10-26 13:03:36 +0300

    initialised dagger and shopDb name

--- app/src/main/java/com/github/didahdx/kyosk/App.kt
+++ app/src/main/java/com/github/didahdx/kyosk/App.kt
@@ -2,16 +2,25 @@ package com.github.didahdx.kyosk
 
 import android.app.Application
 import com.github.didahdx.kyosk.common.TimberLoggingTree
+import com.github.didahdx.kyosk.di.components.AppComponent
+import com.github.didahdx.kyosk.di.components.DaggerAppComponent
+
 import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * Created by Daniel Didah on 10/20/21.
  */
 class App : Application() {
 
+    lateinit var appComponent: AppComponent
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(TimberLoggingTree())
+        appComponent = DaggerAppComponent.builder()
+            .application(this).build()
+        appComponent.inject(this)
     }
 
 }
