3eb132bd8fed59592aa16529b88b6eda2aac711f
DanielDidah <didahdaniel@gmail.com>
2021-10-26 12:56:56 +0300

    recyclerview item data classes

---
+++ app/src/main/java/com/github/didahdx/kyosk/ui/home/RecyclerViewItems.kt
@@ -0,0 +1,38 @@
+package com.github.didahdx.kyosk.ui.home
+
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ * Created by Daniel Didah on 10/22/21.
+ */
+sealed class RecyclerViewItems{
+
+    data class ProductItem(
+        var category: String,
+        var description: String,
+        var id: Int,
+        var image: String,
+        var price: Int,
+        var title: String
+    ): RecyclerViewItems()
+
+    data class CategoryTitle(
+        var code: String,
+        var description: String
+    ): RecyclerViewItems()
+
+    data class CategoryChip(
+        var code: String,
+        var description: String
+    ): RecyclerViewItems()
+
+    data class ProductItemList(
+        var productList: List<ProductItem>
+    ): RecyclerViewItems()
+
+    data class CategoriesChipList(
+        var categoryTitleList: List<CategoryChip>
+    ):RecyclerViewItems()
+
+}
