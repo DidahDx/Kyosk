34d4693869149bec084c8328d0c07c73be11246f
DanielDidah <didahdaniel@gmail.com>
2021-10-26 12:45:52 +0300

    minor update

---
+++ app/src/main/java/com/github/didahdx/kyosk/ui/catergory/CategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.didahdx.kyosk.ui.catergory
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class CategoryViewModel @Inject constructor() : ViewModel() {
+
+}
