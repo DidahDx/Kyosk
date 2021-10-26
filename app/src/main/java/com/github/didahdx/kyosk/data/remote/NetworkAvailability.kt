680111e3dcd5850bde529ed3400057c3f2abc98b
DanielDidah <didahdaniel@gmail.com>
2021-10-26 12:51:14 +0300

    added network check availabilty

---
+++ app/src/main/java/com/github/didahdx/kyosk/data/remote/NetworkAvailability.kt
@@ -0,0 +1,39 @@
+package com.github.didahdx.kyosk.data.remote
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import javax.inject.Inject
+
+/**
+ * Created by Daniel Didah on 10/25/21.
+ */
+class NetworkAvailability @Inject constructor(private val application: Application) {
+
+    @SuppressLint("ObsoleteSdkInt")
+    fun isNetworkAvailable(): Boolean {
+        val connectivityManager=application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork =
+                connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+    }
+}
