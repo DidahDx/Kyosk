5c74cd6f007e602ea70b077d6c2d9d4c869dae2a
DanielDidah <didahdaniel@gmail.com>
2021-10-26 13:09:19 +0300

    added category

--- app/src/main/java/com/github/didahdx/kyosk/ui/catergory/CategoryFragment.kt
+++ app/src/main/java/com/github/didahdx/kyosk/ui/catergory/CategoryFragment.kt
@@ -1,21 +1,107 @@
-package com.github.didahdx.kyosk.ui.catergories
+package com.github.didahdx.kyosk.ui.catergory
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.didahdx.kyosk.App
 import com.github.didahdx.kyosk.R
+import com.github.didahdx.kyosk.common.Resources
+import com.github.didahdx.kyosk.data.local.entities.CategoryEntity
+import com.github.didahdx.kyosk.data.mapper.mapToCategoryTitle
+import com.github.didahdx.kyosk.databinding.CategoryFragmentBinding
+import com.github.didahdx.kyosk.ui.BaseFragment
+import com.github.didahdx.kyosk.ui.extensions.navigateSafe
+import com.github.didahdx.kyosk.ui.extensions.snackBar
+import com.github.didahdx.kyosk.ui.home.HomeFragment
+import com.github.didahdx.kyosk.ui.home.RecyclerViewAdapter
+import com.github.didahdx.kyosk.ui.home.RecyclerViewItems
 
-class CategoriesFragment : Fragment() {
+
+class CategoryFragment : BaseFragment() {
+    companion object {
+        const val categoryTitle = "CategoryTitle"
+    }
+
+//    @Inject
+//    lateinit var viewModel: ViewModelProvider.Factory
+
+    private val categoryViewModel: CategoryViewModel by viewModels()
+    private var _binding: CategoryFragmentBinding? = null
+    private val binding get() = _binding!!
+//    private lateinit var categoryViewModel: CategoryViewModel
+    var categoryTitle: CategoryEntity? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireNotNull(this.activity).application as App).appComponent.inject(this)
+//        categoryViewModel = ViewModelProvider(this, viewModel).get(CategoryViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.categories_fragment, container, false)
+        _binding = CategoryFragmentBinding.inflate(inflater, container, false)
+
+        val recyclerViewAdapter = RecyclerViewAdapter()
+        val gridLayoutManager = GridLayoutManager(binding.root.context, 2)
+        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvCategories.apply {
+//            layoutManager = LinearLayoutManager(binding.root.context)
+            layoutManager = gridLayoutManager
+            adapter = recyclerViewAdapter
+        }
+        recyclerViewAdapter.itemClickListener = { view, item, position ->
+            when (item) {
+                is RecyclerViewItems.CategoriesChipList -> {
+                    //not supported
+                }
+                is RecyclerViewItems.CategoryChip -> {
+                    //not supported
+                }
+                is RecyclerViewItems.CategoryTitle -> {
+                    //not supported
+                }
+                is RecyclerViewItems.ProductItem -> {
+                    val bundle= bundleOf(HomeFragment.productId to item.id)
+                    this.findNavController()
+                        .navigateSafe(R.id.action_categoryFragment_to_productDetailFragment,bundle)
+                }
+                is RecyclerViewItems.ProductItemList -> {
+                    //not supported
+                }
+            }
+        }
+        categoryTitle = arguments?.getParcelable<CategoryEntity>(CategoryFragment.categoryTitle)
+        categoryTitle?.let { categoryViewModel.getAllProductByCategory(it.mapToCategoryTitle()) }
+        categoryViewModel.item.observe(viewLifecycleOwner, { item ->
+            when (item) {
+                is Resources.Error -> {
+                    item.message?.let { binding.root.snackBar(it) }
+                }
+                is Resources.Loading -> {
+                    //loading
+                }
+                is Resources.Success -> {
+                    recyclerViewAdapter.submitList(item.data)
+                }
+            }
+
+        })
+
+        return binding.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/com/github/didahdx/kyosk/ui/catergory/CategoryViewModel.kt
+++ app/src/main/java/com/github/didahdx/kyosk/ui/catergory/CategoryViewModel.kt
@@ -1,8 +1,40 @@
 package com.github.didahdx.kyosk.ui.catergory
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.didahdx.kyosk.common.Resources
+import com.github.didahdx.kyosk.data.repository.CategoryRepository
+import com.github.didahdx.kyosk.ui.home.RecyclerViewItems
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
-class CategoryViewModel @Inject constructor() : ViewModel() {
+class CategoryViewModel @Inject constructor(
+    private val categoryRepository: CategoryRepository
+) : ViewModel() {
 
+    private val compositeDisposable=CompositeDisposable()
+    val item= MutableLiveData<Resources<List<RecyclerViewItems>>>()
+
+    fun getAllProductByCategory(categoryTitle: RecyclerViewItems.CategoryTitle){
+       val disposable= categoryRepository.fetchAllProductsByCategory(categoryTitle)
+           .subscribeOn(Schedulers.io())
+           .observeOn(AndroidSchedulers.mainThread())
+           .subscribe({
+                      item.value= it
+           },{ error->
+               Timber.e(error)
+               item.value=  Resources.Error<List<RecyclerViewItems>>("Something went wrong")
+           })
+        compositeDisposable.add(disposable)
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+        categoryRepository.onClear()
+    }
 }
